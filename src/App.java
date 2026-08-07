a4b66d9bc7d6105ffd64f434a71593c86f6cf348
Carlos Andrade <andracar000@warren.k12.in.us>
2026-08-07 12:53:35 -0400

    initial commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String myName = "Carlos";
+        System.out.println(myName);
+
+        myName = "Andrade";
+        System.out.println(myName);
+
+        char myLetter = '\u00A9';
+        System.out.println(myLetter);
+
+        int x = 3;
+        short y = 3;
+        byte b = 3;
+        long z = 3108970983L;
+
+        double d = 3.1415926538980897;
+        float f = 3.1415926538980897F;
+
+        boolean myBool = true;
+        boolean myBool2 = false;
+
+    }
+}
